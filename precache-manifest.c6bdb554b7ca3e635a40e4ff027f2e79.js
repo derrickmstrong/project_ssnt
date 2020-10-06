e6da337de43fb510617b17e5015d47cdf45b2b3c
Derrick Strong <derrickmstrong@gmail.com>
2020-10-06 13:25:16 -0500

    Updates

---
+++ precache-manifest.c6bdb554b7ca3e635a40e4ff027f2e79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17576d0783de0c132b0199cd334d4583",
+    "url": "/project_ssnt/index.html"
+  },
+  {
+    "revision": "2025b3d8493e37e0137a",
+    "url": "/project_ssnt/static/css/main.cfeb7342.chunk.css"
+  },
+  {
+    "revision": "5acbac39b489a38dadb2",
+    "url": "/project_ssnt/static/js/2.020ca00e.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/project_ssnt/static/js/2.020ca00e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2025b3d8493e37e0137a",
+    "url": "/project_ssnt/static/js/main.829f738f.chunk.js"
+  },
+  {
+    "revision": "4d00621772ca34b370de",
+    "url": "/project_ssnt/static/js/runtime-main.ca399ec1.js"
+  }
+]);
