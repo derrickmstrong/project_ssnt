3386f34ed308442ce35c45721302bba03c6cdd5e
Derrick Strong <derrickmstrong@gmail.com>
2021-02-07 13:48:37 -0600

    Updates

--- precache-manifest.88d84a3d881fbad12529f36a7a89c5f1.js
+++ precache-manifest.88d84a3d881fbad12529f36a7a89c5f1.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "71af2c9fd68388d8a8cfdbecaf5ed7d9",
+    "revision": "4bc8073b26a39ca08d97e7815a9851cd",
     "url": "/project_ssnt/index.html"
   },
   {
-    "revision": "6216f6452355fd53cc21",
+    "revision": "394cf315375a4e01d1c3",
     "url": "/project_ssnt/static/css/main.18526ddc.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/project_ssnt/static/js/2.02475025.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6216f6452355fd53cc21",
-    "url": "/project_ssnt/static/js/main.83ea8092.chunk.js"
+    "revision": "394cf315375a4e01d1c3",
+    "url": "/project_ssnt/static/js/main.36d9a506.chunk.js"
   },
   {
     "revision": "4d00621772ca34b370de",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/project_ssnt/precache-manifest.f83151004bdef03494421809af01ffff.js"
+  "/project_ssnt/precache-manifest.88d84a3d881fbad12529f36a7a89c5f1.js"
 );
 
 self.addEventListener('message', (event) => {
