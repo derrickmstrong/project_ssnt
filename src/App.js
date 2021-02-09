7a9ca3d0e1e981123c02a45e21b2459de6c0c619
Derrick Strong <derrickmstrong@gmail.com>
2021-02-09 11:45:22 -0600

    Updated Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Nav from './components/Nav';
 import Footer from './components/Footer';
